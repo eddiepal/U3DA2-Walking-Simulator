95b94c9727b9a3572e33b296a6d17799efaf643a
Edward Burke <empburke@outlook.com>
2018-04-29 12:30:02 +0100

    stuff

---
+++ WalkingSimulator/Assets/GameStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+
+	public GameObject skulls;
+	public GameObject fences;
+	public GameObject terrain;
+	// Use this for initialization
+	void Start () {
+		skulls.SetActive (true);
+		fences.SetActive (true);
+		terrain.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
