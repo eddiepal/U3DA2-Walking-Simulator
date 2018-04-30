290745ba9435ae53078ae95c70ac903ba8a91899
Edward Burke <empburke@outlook.com>
2018-04-30 07:43:48 +0100

    stuff

---
+++ WalkingSimulator/Assets/Skeleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour {
+
+	public GameObject skeleton;
+	public AudioSource scream;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider other){
+		skeleton.SetActive (true);
+		scream.Play();
+	}
+}
